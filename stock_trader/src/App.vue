1f751493db1a01977354a2e01818151a238407b8
Rainer Kuhn <ticktricktrack@gmail.com>
2016-12-16 06:59:39 +0000

    routing

--- stock_trader/src/App.vue
+++ stock_trader/src/App.vue
@@ -1,12 +1,18 @@
 <template lang="html">
   <div class="container">
-
+    <app-header></app-header>
+    <div class="main-container container">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
+  import Header from './components/header.vue';
   export default {
-
+    components: {
+      appHeader: Header
+    }
   }
 </script>
 
--- stock_trader/src/components/header.vue
+++ stock_trader/src/components/header.vue
@@ -1,5 +1,28 @@
 <template lang="html">
-  <h1>header</h1>
+  <nav class="nav">
+    <div class="nav-left">
+      <router-link to='/' class="nav-item is-large">Stock Trader</router-link>
+    </div>
+
+    <div class="nav-right nav-menu">
+      <router-link to='/stocks' class="nav-item">Stocks</router-link>
+      <router-link to='/portfolio' class="nav-item">Portfolio</router-link>\
+      <span class="nav-item">
+       <a class="button" >
+         <span class="icon">
+           <i class="fa fa-twitter"></i>
+         </span>
+         <span>Tweet</span>
+       </a>
+       <a class="button is-primary" href="#">
+         <span class="icon">
+           <i class="fa fa-download"></i>
+         </span>
+         <span>Download</span>
+       </a>
+     </span>
+   </div>
+  </nav>
 </template>
 
 <script>
