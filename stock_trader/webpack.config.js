35c1461cbf8e57e51e6bd63486f7ced6b7ef2846
Rainer Kuhn <ticktricktrack@gmail.com>
2016-12-17 00:45:42 +0000

    update webpack

--- stock_trader/webpack.config.js
+++ stock_trader/webpack.config.js
@@ -2,6 +2,9 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
+  performance: {
+    hints: false
+  },
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
