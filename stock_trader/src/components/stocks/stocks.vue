ea2c50add2bddd4c12addc51ac1d546d4d803b1a
Rainer Kuhn <ticktricktrack@gmail.com>
2016-12-18 12:36:59 +0000

    modularize

--- stock_trader/src/components/stocks/stocks.vue
+++ stock_trader/src/components/stocks/stocks.vue
@@ -7,18 +7,13 @@
 <script>
   import Stock from './stock.vue';
   export default {
-    data() {
-      return {
-        stocks: [
-          { id: 1, name: 'Apple', price: 100 },
-          { id: 2, name: 'Google', price: 130 },
-          { id: 3, name: 'Facebook', price: 200 },
-          { id: 4, name: 'Twitter', price: 49 }
-        ]
-      }
-    },
     components: {
       appStock: Stock
+    },
+    computed: {
+      stocks() {
+        return this.$store.getters.stocks;
+      }
     }
   }
 </script>
---
+++ stock_trader/src/store/modules/stocks.js
@@ -0,0 +1,39 @@
+import stocks from '../data/seed';
+
+const state = {
+  stocks: []
+};
+
+const getters = {
+  stocks: state => {
+    return state.stocks;
+  }
+};
+
+const mutations = {
+  setStocks(state, stocks) {
+    state.stocks = stocks;
+  },
+  random(state) {
+
+  }
+};
+
+const actions = {
+  initStocks: ({commit}) => {
+      commit('setStocks', stocks);
+  },
+  buy: ({ commit }, order) => {
+    commit();
+  },
+  randomize: ({ commit }) => {
+    commit('random');
+  }
+};
+
+export default {
+    state,
+    mutations,
+    actions,
+    getters
+};
